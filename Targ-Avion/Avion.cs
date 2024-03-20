908dc23bd5297a94690d58f24d45c495cb078d4a
al12us <rusalexis172@gmail.com>
2024-03-20 20:19:38 +0200

    Avion actualizare

--- Targ-Avion/Avion.cs
+++ Targ-Avion/Avion.cs
@@ -8,44 +8,23 @@ namespace Targ_Avion
 {
     internal class Avion
     {
-        public string firma { set; get; }
-        public string model { set; get; }
-        public int an_fabricatie { set; get; }
-        public string culoare { set; get; }
-        public double greutate { set; get; }
-        public double pret { set; get; }
-        public int nr_de_pasageri { set; get; }
-        public Avion()
-        {
-            firma = model= string.Empty;
-        
-            an_fabricatie = 0;
-            culoare = string.Empty;
-            greutate = 0;
-            pret = 0;
-            nr_de_pasageri = 0;
+        private string v1;
+        private string v2;
+        private int v3;
+        private string v4;
+        private double v5;
+        private double v6;
+        private int v7;
 
-        }
-        public Avion(string firma,string model,int an_fabricatie,string culoare,double greutate,double pret,int nr_de_pasageri)
+        public Avion(string v1, string v2, int v3, string v4, double v5, double v6, int v7)
         {
-            this.firma = firma;
-            this.model = model;
-            this.an_fabricatie = an_fabricatie;
-            this.culoare = culoare;
-            this.greutate = greutate;
-            this.pret = pret;
-            this.nr_de_pasageri = nr_de_pasageri;
-
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+            this.v7 = v7;
         }
-        public string Info()
-        {
-            string afisare = $"Firma:{firma ?? "Necunoscut"}\n Model:{model  ?? "necunoscut"}\n Anul in care este fabricat {an_fabricatie} \n Culoarea:{culoare ?? "necunoscut"}\n" +
-                             $"Greutatea:{greutate}\n Pret:{pret}\n Numar de pasageri{nr_de_pasageri}";
-            return afisare;
-            
-        }
-
-        
-        
     }
 }
--- Targ-Avion/Program.cs
+++ Targ-Avion/Program.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Avion;
+using Avion;
 namespace Targ_Avion
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Avion obiect = new Avion ("TAROM","B-20",2010,"violet",105.5,1000.16,1000);
-            obiect.Info();
+            Avion obiect = new Avion("TAROM","B-20",2010,"violet",105.5,1000.16,1000);
+           Console.WriteLine(obiect.Info());
           
         } 
     }
