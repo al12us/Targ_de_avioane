f48cfb60fe2d9f0ac51a1833c342a9291a7cb30e
al12us <rusalexis172@gmail.com>
2024-03-24 12:07:22 +0200

    S-a completat adminstrarea avioaneMemorie cu ce s-a facut in lab

---
+++ Niveldestocare_Date/AdministrareaAvioaneMemorie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avion;
+namespace Niveldestocare_Date
+{
+    public class AdministrareAvioane_Memorie
+    {
+        private const int NR_MAX_AVIOANE = 50;
+
+        private  AvionClass[] avioane;
+        private int nr_avioane;
+        public AdministrareAvioane_Memorie()
+        {
+             avioane= new AvionClass[NR_MAX_AVIOANE];
+             nr_avioane = 0;
+        }
+
+        public void AddAvion(AvionClass avionul)
+        {
+            avioane[nr_avioane] = avionul;
+            nr_avioane++;
+        }
+
+        public AvionClass[] GetAvioane(out int nr_avioane)
+        {
+            nr_avioane = this.nr_avioane;
+            return avioane;
+        }
+    }
+}
--- Targ-Avion/Program.cs
+++ Targ-Avion/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avion;
+using Niveldestocare_Date;
 namespace Targ_Avion
 {
     internal class Program
