6b6a71421d4aedfe6491338b107a8a4c528ce5f8
al12us <rusalexis172@gmail.com>
2024-04-01 23:22:22 +0300

    Actualizare memorie si fisier pentru producator de avioane +adaugareProducatordeavioane.txt in App.config

--- Niveldestocare_Date/AdministrareProducatorMemorie.cs
+++ Niveldestocare_Date/AdministrareProducatorMemorie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProducatorAvioane;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace Niveldestocare_Date
 {
-    internal class AdministrareProducatorMemorie
+    public class AdministrareProducatorMemorie
     {
+        private const int NR_MAX_PRODUCATORI = 50;
+        private ProductAvion[] producatori;
+        private int nr_producatori;
+
+        public AdministrareProducatorMemorie()
+        {
+            producatori = new ProductAvion[NR_MAX_PRODUCATORI];
+            nr_producatori = 0;
+        }
+        public void AddProducator(ProductAvion producatorul)
+        {
+            producatori[nr_producatori] = producatorul;
+            nr_producatori++;
+        }
+        public ProductAvion[] GetProducator(out int nr_producatori)
+        {
+            nr_producatori=this.nr_producatori;
+            return producatori;
+
+        }
     }
 }
--- Niveldestocare_Date/AdministratorProducator_FisierText.cs
+++ Niveldestocare_Date/AdministratorProducator_FisierText.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProducatorAvioane;
+using System.IO;
+using Avion;
 namespace Niveldestocare_Date
 {
-    internal class AdministratorProducator_FisierText
+    public class AdministratorProducator_FisierText
     {
+
+        private const int NR_MAX_PRODUCATORI = 50;
+        private string numeFisier_2;
+        public AdministratorProducator_FisierText(string numeFisier_2)
+        {
+            this.numeFisier_2 = numeFisier_2;
+            // se incearca deschiderea fisierului in modul OpenOrCreate
+            // astfel incat sa fie creat daca nu exista
+            Stream streamFisierText = File.Open(numeFisier_2, FileMode.OpenOrCreate);
+            streamFisierText.Close();
+        }
+        public void AddProducator(ProductAvion producator)
+        {
+            // instructiunea 'using' va apela la final streamWriterFisierText.Close();
+            // al doilea parametru setat la 'true' al constructorului StreamWriter indica
+            // modul 'append' de deschidere al fisierului
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier_2, true))
+            {
+                //streamWriterFisierText.WriteLine(producator.ConversieSir_PentruFisier());
+            }
+        }
+        public ProductAvion[] GetProducts(out int nr_producatori)
+        {
+            ProductAvion[] producatori = new ProductAvion[NR_MAX_PRODUCATORI]; 
+            // instructiunea 'using' va apela streamReader.Close()
+            using (StreamReader streamReader = new StreamReader(numeFisier_2))
+            {
+                string linieFisierB;
+                nr_producatori= 0;
+
+                // citeste cate o linie si creaza un obiect de tip Student
+                // pe baza datelor din linia citita
+                while ((linieFisierB = streamReader.ReadLine()) != null)
+                {
+                    producatori[nr_producatori++] = new ProductAvion(linieFisierB);
+                }
+            }
+
+            return producatori;
+        }
     }
 }
