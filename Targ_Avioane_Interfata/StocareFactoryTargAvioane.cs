81a0a13687582a47033ff59228d5bc9c921c3b76
al12us <rusalexis172@gmail.com>
2024-07-21 13:25:13 +0300

    Adaugare IStocareDate pentru interfata si Stocare Factory

---
+++ Targ_Avioane_Interfata/StocareFactoryTargAvioane.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niveldestocare_Date;
+
+namespace Targ_Avioane_Interfata
+{
+    public class StocareFactoryTargAvioane
+    {
+        private const string FORMAT_SALVARE = "FormatSalvare";
+        private const string NUME_FISIER = "NumeFisier";
+        public static IStocareDate GetAdministratorStocare()
+        {
+            string formatSalvare = ConfigurationManager.AppSettings[FORMAT_SALVARE];
+
+            string numeFisier = ConfigurationManager.AppSettings[NUME_FISIER];
+            string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    default:
+                    //case "bin":
+                       // return new AdministrareStudenti_FisierBinar(caleCompletaFisier + "." + formatSalvare);
+                    case "txt":
+                        return new AdministrareAvioane_FisierText(caleCompletaFisier + "." + formatSalvare);
+                }
+            }
+
+            return null;
+        }
+    }
+
+}
+
